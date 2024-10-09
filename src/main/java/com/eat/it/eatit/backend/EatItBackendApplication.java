adff34cdffeef511577d9380debacf89e8dae320
kamil <kamil.sakowicz@onet.pl>
2024-10-09 18:18:59 +0200

    Remove functionality from main app

--- src/main/java/com/eat/it/eatit/backend/EatItBackendApplication.java
+++ src/main/java/com/eat/it/eatit/backend/EatItBackendApplication.java
@@ -1,9 +1,5 @@
 package com.eat.it.eatit.backend;
 
-import com.eat.it.eatit.backend.account.data.Account;
-import com.eat.it.eatit.backend.fridge.data.Fridge;
-import com.eat.it.eatit.backend.account.data.AccountRepository;
-import com.eat.it.eatit.backend.fridge.data.FridgeRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -13,19 +9,7 @@ public class EatItBackendApplication {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(EatItBackendApplication.class, args);
-        AccountRepository repo = context.getBean(AccountRepository.class);
-        FridgeRepository fridgeRepository = context.getBean(FridgeRepository.class);
 
-        Account account = new Account();
-        Fridge fridge = new Fridge();
-        System.out.println(fridge);
-        account.setUsername("kamil");
-        account.setMail("kamil@mail.com");
-        account.setFridge(fridge);
-        Account savedAcc = repo.save(account);
-        fridge.setOwnerId(savedAcc.getId());
-        fridgeRepository.save(fridge);
-        System.out.println(account);
     }
 
 }
