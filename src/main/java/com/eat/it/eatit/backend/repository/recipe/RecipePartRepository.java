1ad461bfb48443a856d2b873b51b0a65b39f4d04
sztrok <kamil.sakowicz@onet.pl>
2025-02-05 14:20:08 +0100

    Add repositories

---
+++ src/main/java/com/eat/it/eatit/backend/repository/recipe/RecipePartRepository.java
@@ -0,0 +1,9 @@
+package com.eat.it.eatit.backend.repository.recipe;
+
+import com.eat.it.eatit.backend.data.refactored.recipe.RecipePart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecipePartRepository extends JpaRepository<RecipePart, Long> {
+}
